bfd6eeca0a26d4a5b442ea9a8163743dea7c017b
ten852456 <ten.palaboon@gmail.com>
2026-04-14 21:43:25 +0700

    feat: initialize root layout with fonts, metadata, and global components

--- landing-v2/src/app/layout.tsx
+++ landing-v2/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { DM_Sans, Exo_2 } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import NavTicker from "@/components/NavTicker";
@@ -46,6 +47,18 @@ export default function RootLayout({
       className={`${dmSans.variable} ${exo2.variable} h-full antialiased`}
     >
       <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YJYVH5BZJZ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YJYVH5BZJZ');
+          `}
+        </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
