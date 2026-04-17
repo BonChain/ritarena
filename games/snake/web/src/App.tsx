e1b4c77b7b11243777012b951b6668a6983265fb
ten852456 <ten.palaboon@gmail.com>
2026-04-17 15:56:07 +0700

    feat(snake): bootstrap React web bundle with Vite + @ritarena/ui

---
+++ games/snake/web/src/App.tsx
@@ -0,0 +1,10 @@
+export function App() {
+  return (
+    <div className="snake-layout">
+      <div className="snake-header">Snake Royale (scaffold)</div>
+      <div className="snake-canvas-column">Canvas goes here</div>
+      <div className="snake-sidebar">Sidebar goes here</div>
+      <div className="snake-controls">Controls go here</div>
+    </div>
+  );
+}
